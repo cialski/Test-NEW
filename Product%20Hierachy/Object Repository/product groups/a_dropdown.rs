d6933699e7773b2b8bbc8aafdcff60d58f674784
cial <49693100+cialski@users.noreply.github.com>
2025-02-19 14:16:59 +0800

    Add files via upload

---
+++ Product%20Hierachy/Object Repository/product groups/a_dropdown.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_dropdown</name>
+   <tag></tag>
+   <elementGuidId>b5fdd08e-e00c-42d4-907e-7f60a6430222</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[text() = '${text}']/parent::span/preceding-sibling::span/a</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
